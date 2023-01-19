db647f494dd0b599ecd0e0d61081f05c5d531536
luciaumbreit <luciaumbreit@localhost>
2023-01-19 12:14:03 -0600

    Message has been changed

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Say hi Luciaaaa!");
+		System.out.println("Happy Ethan Birthday day to all who celebrate!");
 	}
 }
